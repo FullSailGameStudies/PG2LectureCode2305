336321c128cb702be59ae9e1a504970d69d4f011
Garrett Girod <girodg@hotmail.com>
2023-05-08 09:58:19 -0400

    bubble sort

--- Day04/Day04/Program.cs
+++ Day04/Day04/Program.cs
@@ -7,42 +7,51 @@ namespace Day04
 {
     internal class Program
     {
+        static void BubbleSort(List<string> unsorted)
+        {
+            int count = unsorted.Count;
+            bool swapped;
+            do
+            {
+                swapped = false;
+                for (int i = 1; i < count; i++)
+                {
+                    int comp = unsorted[i - 1].CompareTo(unsorted[i]);
+                    if (comp > 0)
+                    {
+                        /*
+                        int temp = unsorted[i - 1];
+                        unsorted[i-1] = unsorted[i];
+                        unsorted[i] = temp;
+                         */
+                        (unsorted[i], unsorted[i-1]) = (unsorted[i-1], unsorted[i]);
+                        swapped = true;
+                    }
+                }
+                --count;
+            } while (swapped);
+        }
         static void Main(string[] args)
         {
-            //Recursion, Sorting, Searching
+            string s1 = "Batman", s2 = "Aquaman";
+            //CompareTo
+            //  -1  LESS THAN
+            //   0  EQUAL TO
+            //   1  GREATER THAN
+            int compareResult = s1.CompareTo(s2);
+            if(compareResult < 0) 
+                Console.WriteLine($"{s1} is LESS THAN {s2}");
+            else if (compareResult > 0)
+                Console.WriteLine($"{s1} is GREATER THAN {s2}");
+            else
+                Console.WriteLine($"{s1} is EQUAL TO {s2}");
 
-            /*
-                ╔═════════╗ 
-                ║Recursion║
-                ╚═════════╝ 
-             
-                Recursion happens when a method calls itself. This creates a recursive loop.
-                
-                All recursive methods need an exit condition, something that prevents the loop from continuing.
-              
-            */
-            int N = 0;
-            RecursiveLoop(N);
-            Console.ResetColor();
-
-
-            /*
-                CHALLENGE 1:
-
-                    convert this for loop to a recursive method called Bats. Call Bats here in Main.
-             
-                    for(int i = 0;i < 100;i++)
-                    {
-                        Console.Write((char)78);
-                        Console.Write((char)65);
-                        Console.Write(' ');
-                    }
-            */
-
-
-
-
-
+            List<string> A = new() { "Wonder Woman", "Batman", "Superman", "Flash", "Aquaman", "Blue Beetle", "Lobo" };
+            foreach (var i in A) Console.Write($"{i} ");
+            Console.WriteLine();
+            BubbleSort(A);
+            foreach (var i in A) Console.Write($"{i} ");
+            Console.ReadKey();
             /*
                 ╔═══════╗ 
                 ║Sorting║
@@ -50,7 +59,7 @@ namespace Day04
              
                 Sorting is used to order the items in a list/array is a specific way
              
-                CHALLENGE 2:
+                CHALLENGE:
 
                     Convert this BubbleSort pseudo-code into a C# method             
                      
@@ -69,6 +78,40 @@ namespace Day04
                     end procedure
                     
             */
+
+
+
+
+
+
+            /*
+                ╔═════════╗ 
+                ║Recursion║
+                ╚═════════╝ 
+             
+                Recursion happens when a method calls itself. This creates a recursive loop.
+                
+                All recursive methods need an exit condition, something that prevents the loop from continuing.
+              
+            */
+            int N = 0;
+            RecursiveLoop(N);
+            Console.ResetColor();
+
+
+            /*
+                CHALLENGE:
+
+                    convert this for loop to a recursive method called Bats. Call Bats here in Main.
+             
+                    for(int i = 0;i < 100;i++)
+                    {
+                        Console.Write((char)78);
+                        Console.Write((char)65);
+                        Console.Write(' ');
+                    }
+            */
+
 
         }
 
